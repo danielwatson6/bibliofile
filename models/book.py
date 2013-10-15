1bcef1ab488c3b17f686b51630261edb221fb754
Daniel Watson <watsondaniel6@gmail.com>
2013-10-14 22:58:06 -0500

    Added more fields

--- models/book.py
+++ models/book.py
@@ -3,11 +3,31 @@ from lib import db
 # Book model
 class BookModel(db.BlobModel):
 	
-	# Issue: add more data here
-	title = db.string(required = True)
-	description = db.text(default = '')
+	### Data
 	
-	def small_description(self, max_len = 30):
+	# Main data
+	title = db.string(required = True)
+	author = db.string(default = "Unknown")
+	submitter = db.string(default = "Anonymus")
+	
+	# Dates
+	submitted = db.date(auto_now_add = True)
+	last_edit = db.date(auto_now_add = True) #TODO: fix this
+	
+	# Descriptions
+	description = db.text(default = '')
+	info_description = db.text(default = '')
+	author_description = db.text(default = '')
+	
+	# Other data
+	book_img = db.string(default = 'javascript:void(0)')
+	author_img = db.string(default = 'javascript:void(0)')
+	
+	
+	### Functions
+	
+	# For rendering purposes
+	def small_description(self, max_len = 300):
 		try:
 			return self.description[:max_len]
 		except:
