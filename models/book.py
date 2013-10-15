e7eecf5ef3c3156111167e205aafa3aabe1ec972
Daniel Watson <watsondaniel6@gmail.com>
2013-10-14 20:43:54 -0500

    Added description field

--- models/book.py
+++ models/book.py
@@ -5,3 +5,10 @@ class BookModel(db.BlobModel):
 	
 	# Issue: add more data here
 	title = db.string(required = True)
+	description = db.text(default = '')
+	
+	def small_description(self, max_len = 30):
+		try:
+			return self.description[:max_len]
+		except:
+			return self.description
