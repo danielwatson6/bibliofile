29bc88e4f2bb9c06e512e6570c9c0f23137f9504
Daniel Watson <watsondaniel6@gmail.com>
2013-10-25 19:11:24 -0500

    Added more front-side validation
    Includes support for extensions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,11 @@
  * external tampering with the
  * undefined value.
  **/
+ 
+// Extensions allowed in blob uploads 
+EXTENSIONS = ['pdf']
+ 
+ 
 !function (window, undefined) {
 	
 	$(document).ready(function () {
@@ -18,23 +23,45 @@
 function validate_upload() {
 	
 	var title = $('#title');
-	var blob = $('#blob');
+	var blob  = $('#blob');
 	
-	if (title.val() != '' && blob.val() != '') {
+	title_test = title.val() != ''
+	blob_test = blob.val() != ''
+	extension_test =  test_extension(blob.val())
+	
+	if (title_test && blob_test && extension_test) {
 		return true;
 	}
 	
 	// Add error messages
-	if (!title.val()) {
+	if (!title_test) {
 		$('#titleError').html("Please add a title.");
 	} else {
 		$('#titleError').html("");
 	}
-	if (!blob.val()) {
+	if (!blob_test) {
 		$('#blobError').html("Please upload a file.");
 	} else {
-		$('#blobError').html("");
+		if (!extension_test) {
+			$('#blobError').html("Please select a file with a valid extension.");
+		} else {
+			$('#blobError').html("");
+		}
 	}
 	
 	return false;
+}
+
+function test_extension(s) {
+	if (s != undefined && s != '' && s != null) {
+		extension = s.split('.')[s.split('.').length-1];
+		for (var i = 0; i < EXTENSIONS.length; i++) {
+			if (EXTENSIONS[i] == extension) {
+				return true;
+			}
+		}
+		return false;
+	} else {
+		return false;
+	}
 }
