95a7c603e927900f42eecac214803b68d2fb3d4a
Daniel Watson <watsondaniel6@gmail.com>
2013-11-06 15:00:49 -0600

    Added fair backend check

--- controllers/book_upload.py
+++ controllers/book_upload.py
@@ -13,10 +13,20 @@ class Book(UploadController):
 	def create(self):
 		
 		# Get all params from the html form
-		data = self.get_data("title", "blob")
+		data = self.get_data("title",
+		                     "blob",
+		                     "author",
+		                     "genre",
+		                     "description",
+		                     "author_description")
 		
-		if data["title"] and data["blob"]:
+		if data["title"] and data["blob"] and data["author"] and data["genre"] \
+		   and data["description"] and data["author_description"]:
 			
 			# HTML-Safe
 			data["title"] = utils.escape(data["title"])
+			data["author"] = utils.escape(data["author"])
+			data["genre"] = utils.escape(data["genre"])
+			data["description"] = utils.escape(data["description"])
+			data["author_description"] = utils.escape(data["author_description"])
 			return data
